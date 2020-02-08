b2216a9a6afda5632b7a7548fac4cb180498e19c
User <User@DESKTOP-IBGB2Q9>
2020-02-08 14:49:06 +0200

    new apdate

--- IsraaAhmad/src/IsraaAhmad.java
+++ IsraaAhmad/src/IsraaAhmad.java
@@ -4,6 +4,8 @@ public class IsraaAhmad {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		 System.out.print("hi soso");
+		 System.out.print("bye soso");
+		 
 	}
 
 }
